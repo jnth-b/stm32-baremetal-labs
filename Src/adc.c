5b8331bad468db29c1756538a29705f9afbda04a
jnth-b <268634369+jnth-b@users.noreply.github.com>
2026-04-08 21:25:25 +0530

    Clean up ADC register bits for readability
    Switched magic numbers to named bit macros and made the continuous conversion setup easier to scan during debug sessions.

--- Src/adc.c
+++ Src/adc.c
@@ -1,24 +1,32 @@
 #include "stm32f4xx.h"
 #include "adc.h"
 
+#define GPIOAEN      (1U << 0)
+#define ADC1EN       (1U << 8)
+#define ADC_CH1      1U
+#define ADC_CONT     (1U << 1)
+#define ADC_ADON     (1U << 0)
+#define ADC_SWSTART  (1U << 30)
+#define ADC_EOC      (1U << 1)
+
 void pa1_adc_init(void)
 {
-    RCC->AHB1ENR |= (1U << 0);
+    RCC->AHB1ENR |= GPIOAEN;
     GPIOA->MODER |= (1U << 2) | (1U << 3);
-    RCC->APB2ENR |= (1U << 8);
-    ADC1->SQR3 = 1;
+    RCC->APB2ENR |= ADC1EN;
+    ADC1->SQR3 = ADC_CH1;
     ADC1->SQR1 = 0;
-    ADC1->CR2 |= (1U << 0);
+    ADC1->CR2 |= ADC_ADON;
 }
 
 void start_conversion(void)
 {
-	ADC1->CR2 |= (1U << 1);
-    ADC1->CR2 |= (1U << 30);
+    ADC1->CR2 |= ADC_CONT;
+    ADC1->CR2 |= ADC_SWSTART;
 }
 
 uint32_t adc_read(void)
 {
-    while(!(ADC1->SR & (1U << 1))) {}
+    while (!(ADC1->SR & ADC_EOC)) {}
     return ADC1->DR;
 }
